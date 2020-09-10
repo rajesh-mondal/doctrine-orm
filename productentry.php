7468f9d29ea7592847cc33a09011b12b84081615
Rajesh Mondal <rajesh01749@gmail.com>
2020-09-10 14:10:06 +0600

    product entry added

--- productentry.php
+++ productentry.php
@@ -1,14 +1,16 @@
 <?php
 
 use Shop\Model\Product;
+use Shop\Model\User;
 
 require "bootstrap.php";
 
-$user = $entityManager->find( 'User', 1 );
-$newProductName = "CPU 4GB";
+$user = $entityManager->find( User::class, 2 );
+$newProductName = "Pendrive 32 GB";
 
 $product = new Product;
 $product->setName( $newProductName );
+$product->setUser( $user );
 
 $entityManager->persist( $product );
 $entityManager->flush();
